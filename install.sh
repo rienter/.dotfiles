cccf27a2866c607062282d968cf4fe561e8f2996
rienter <rickyrie98ma@gmail.com>
2025-04-14 22:33:23 +0200

    some small changes

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-APPLICATIONS="neovim zen-browser tmux onlyoffice-bin git"
+APPLICATIONS="neovim zen-browser tmux onlyoffice-bin git go"
 
+# clean
+echo "Cleaning existing directories"
+find ./config -mindepth 1 -maxdepth 1 -type d \
+	-execdir rm -rfv ~/.config/{} \;
+rm -rfv ~/.bin
+
+# make links
+echo "Creating links"
 find ./config -mindepth 1 -maxdepth 1 -type d \
 	-execdir ln -srdv {} ~/.config/{} \;
+ln -srdv bin ~/.bin
